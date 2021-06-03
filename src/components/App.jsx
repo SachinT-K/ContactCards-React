bfc47fb21614b27675b3b0e44dca4595fe097d2b
SachinT-K <75876472+SachinT-K@users.noreply.github.com>
2021-06-03 19:03:36 +0530

    Created new function called "createdCard " and mapped that function to contacts file to created cards.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,28 +2,24 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
+function createCard(contacts) {
+  return (
+    <Card
+      id={contacts.id}
+      key={contacts.id}
+      name={contacts.name}
+      img={contacts.img}
+      phone={contacts.phone}
+      email={contacts.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].img}
-        phone={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].img}
-        phone={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].img}
-        phone={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {contacts.map(createCard)}
     </div>
   );
 }
